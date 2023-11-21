6e20252f00bb166f40912991fb4324e001d75655
Felipe Moran <felipmoran3@gmail.com>
2023-11-21 13:59:19 -0500

    creacion modelos

--- gestionPasajeros/models.py
+++ gestionPasajeros/models.py
@@ -4,18 +4,20 @@ from django.dispatch import receiver
 from django.urls import reverse
 
 # Create your models here.
+
+"""
 class Selection(models.Model):
-    """ seleccion de futbol  """
+    
     name = models.CharField(max_length=50)
     shield = models.ImageField(upload_to='shields/')
     team = models.ImageField(upload_to='teams/')
     pub_date = models.DateField(auto_now_add=True)
 
-  a  def __str__(self):
+    def __str__(self):
         return self.name
 
 class Player(models.Model):
-    """ Jugadores """
+    
     selection = models.ForeignKey('Selection', on_delete=models.PROTECT,related_name='get_players' )
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -28,28 +30,41 @@ class Player(models.Model):
     def __str__(self):
         return self.first_name + " " + self.last_name
     
+    """
+    
       # ----------------------------------------------------------------
 
 class Aeropuerto(models.Model):
-    codigo = models.CharField(max_length=3, unique=True)
+    """  Aeropuertos  """
+    codigo = models.IntegerField(max_length=3, unique=True)
     nombre = models.CharField(max_length=100)
     ciudad = models.CharField(max_length=100)
+    foto_aeropuerto = models.ImageField(upload_to='foto_aeropuerto/')
 
     def __str__(self):
         return self.codigo
 
 class Vuelo(models.Model):
-    numero_vuelo = models.CharField(max_length=10)
+    """  Vuelos """
+    numero_vuelo = models.IntegerField(max_length=5, unique=True)
     origen = models.CharField(max_length=100)
     destino = models.CharField(max_length=100)
-    codAeropuerto = models.ForeignKey(Aeropuerto, on_delete=models.CASCADE)
+    cod_aeropuerto = models.ForeignKey(Aeropuerto, on_delete=models.CASCADE)
 
     
     def __str__(self):
         return self.numero_vuelo
 
 class Pasajero(models.Model):
-    codigoPasajero = models.CharField(max_length=100)
-    numero_vuelo = models.ForeignKey(Vuelo, on_delete=models.CASCADE)
+    """  Pasajeros  """
+    codigo_pasajero = models.IntegerField()
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    celular = models.IntegerField()
+    correo = models.CharField(max_length=100)
+    num_vuelo = models.ForeignKey(Vuelo, on_delete=models.CASCADE)
+
+
+   
     def __str__(self):
-        return self.codigoPasajero
+        return self.codigo_pasajero
