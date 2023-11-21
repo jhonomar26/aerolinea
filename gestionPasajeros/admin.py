bf6ade09cab6db7f9324d05ba163f801b02b0041
Felipe Moran <felipmoran3@gmail.com>
2023-11-21 17:15:13 -0500

    creacion CRUD facil, views, rutas, templates, register en admin y archivo requirements

--- gestionPasajeros/admin.py
+++ gestionPasajeros/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from gestionPasajeros.models import Aeropuerto, Vuelo, Pasajero
 
 # Register your models here.
+admin.site.register(Aeropuerto)
+admin.site.register(Vuelo)
+admin.site.register(Pasajero)
